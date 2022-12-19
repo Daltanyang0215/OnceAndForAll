a5f134139c9ce6b5965e9006ecca0969cab1e989
Daltanyang0215 <taxz0215@gmail.com>
2022-12-19 22:17:53 +0900

    리프트 렌즈 추가
    리프트 렌즈 추가

--- Assets/02.Scripts/03.Tower/Towers/Lift.cs
+++ Assets/02.Scripts/03.Tower/Towers/Lift.cs
@@ -8,7 +8,19 @@ public class Lift : TowerBase , IInteraction
     [SerializeField] private float _height;
     [SerializeField] private CapsuleCollider _activeCollider;
 
+    [SerializeField] private GameObject _lense;
+    [SerializeField] private float _lenseSpeed;
+    
+    private Camera _camera;
+
     private bool _active = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _camera = Camera.main;
+    }
+
     public void Interaction()
     {
         if (_active == false)
@@ -19,6 +31,8 @@ public class Lift : TowerBase , IInteraction
             _activeCollider.enabled = true;
             Player.Instance.gameObject.transform.position = transform.position + Vector3.up * _height;
             _plate.transform.position += Vector3.up * (_height);
+
+            _lense.SetActive(true);
         }
         else
         {
@@ -27,6 +41,17 @@ public class Lift : TowerBase , IInteraction
             _activeCollider.enabled = false;
             Player.Instance.gameObject.transform.position = transform.position;
             _plate.transform.localPosition = Vector3.zero;
+
+            _lense.SetActive(false);
+            _lense.transform.eulerAngles = Vector3.right * 90f;
+        }
+    }
+
+    protected override void Update()
+    {
+        if (_active)
+        {
+            _lense.transform.rotation = Quaternion.Lerp(_lense.transform.rotation,_camera.transform.rotation, _lenseSpeed*Time.deltaTime);
         }
     }
 
